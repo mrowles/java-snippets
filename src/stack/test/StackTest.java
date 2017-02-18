bc66c9bf711005b6367d0720cf65d94411e46c1a
Matt Rowles <matthewrowles@gmail.com>
2017-02-18 17:15:29 +1100

    Adds Stacks

---
+++ src/stack/test/StackTest.java
@@ -0,0 +1,60 @@
+package stack.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import stack.main.Stack;
+
+public class StackTest {
+
+    private Stack<String> stack;
+
+    @Before
+    public void setup() {
+        stack = new Stack<String>();
+        stack.push("First in");
+        stack.push("second in");
+        stack.push("Third in");
+        stack.push("Fourth in");
+        stack.push("Fifth in");
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertFalse(stack.isEmpty());
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(stack.size(), 5);
+    }
+
+    @Test
+    public void testPush() {
+        assertEquals(stack.size(), 5);
+        assertEquals(stack.peek(), "Fifth in");
+
+        stack.push("Sixth in");
+
+        assertEquals(stack.size(), 6);
+        assertEquals(stack.peek(), "Sixth in");
+    }
+
+    @Test
+    public void testPop() {
+        assertEquals(stack.size(), 5);
+        assertEquals(stack.peek(), "Fifth in");
+
+        stack.pop();
+
+        assertEquals(stack.size(), 4);
+        assertEquals(stack.peek(), "Fourth in");
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(stack.toString(), "Fifth in Fourth in Third in second in First in ");
+    }
+
+}

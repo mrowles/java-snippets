34e0aa9eaff059329d061f75b096129b6c83dcf8
Matt Rowles <matthewrowles@gmail.com>
2019-09-28 15:51:40 +1000

    Adds square root

---
+++ src/main/java/squareRoot/SquareRoot.java
@@ -0,0 +1,16 @@
+package squareRoot;
+
+public class SquareRoot {
+    public static double getSquareRoot(int number) {
+        double temp;
+
+        double sqrt = number / 2;
+
+        do {
+            temp = sqrt;
+            sqrt = (temp + (number / temp)) / 2;
+        } while ((temp - sqrt) != 0);
+
+        return sqrt;
+    }
+}

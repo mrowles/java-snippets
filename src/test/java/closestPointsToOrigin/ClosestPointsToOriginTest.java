16cab8d22a786ba502222915c42bc96941ac5e90
Matt Rowles <matthewrowles@gmail.com>
2019-09-30 07:12:00 +1000

    Adds Divide and Conquer solution

--- src/test/java/closestPointsToOrigin/ClosestPointsToOriginTest.java
+++ src/test/java/closestPointsToOrigin/ClosestPointsToOriginTest.java
@@ -7,11 +7,11 @@ import org.junit.Test;
 public class ClosestPointsToOriginTest {
 
     @Test
-    public void testSort() {
+    public void testFindClosestSort() {
         int[][] expected = new int[][]{
                 new int[]{1, 2}
         };
-        int[][] result = ClosestPointsToOrigin.sort(
+        int[][] result = ClosestPointsToOrigin.findClosestSort(
                 new int[][]{
                         new int[]{1, 2},
                         new int[]{2, 3},
@@ -27,7 +27,7 @@ public class ClosestPointsToOriginTest {
                 new int[]{4, 3},
                 new int[]{9, 9},
         };
-        result = ClosestPointsToOrigin.sort(
+        result = ClosestPointsToOrigin.findClosestSort(
                 new int[][]{
                         new int[]{4, 3},
                         new int[]{9, 9}
@@ -38,4 +38,36 @@ public class ClosestPointsToOriginTest {
         assertArrayEquals(expected, result);
     }
 
+    @Test
+    public void testFindClosestDivideAndConquer() {
+        ClosestPointsToOrigin closestPointsToOrigin = new ClosestPointsToOrigin();
+        int[][] expected = new int[][]{
+                new int[]{1, 2}
+        };
+        int[][] result = closestPointsToOrigin.findClosestDivideAndConquer(
+                new int[][]{
+                        new int[]{1, 2},
+                        new int[]{2, 3},
+                        new int[]{4, 3},
+                        new int[]{9, 9}
+                },
+                1
+        );
+
+        assertArrayEquals(expected, result);
+
+        expected = new int[][]{
+                new int[]{4, 3},
+                new int[]{9, 9},
+        };
+        result = closestPointsToOrigin.findClosestDivideAndConquer(
+                new int[][]{
+                        new int[]{4, 3},
+                        new int[]{9, 9}
+                },
+                2
+        );
+
+        assertArrayEquals(expected, result);
+    }
 }

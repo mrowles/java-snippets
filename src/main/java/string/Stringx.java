32b8dbf79e63012ff10fc58ec167d75bf6ad7061
Matt Rowles <matthewrowles@gmail.com>
2019-09-30 18:22:17 +1000

    Adds strstr iterative

--- src/main/java/string/Stringx.java
+++ src/main/java/string/Stringx.java
@@ -8,4 +8,30 @@ public class Stringx {
 
         return reverse(str.substring(1)) + str.charAt(0);
     }
+
+    public static int strstrIterative(String primaryStr, String subStr) {
+        if (subStr.length() > primaryStr.length()) {
+            return -1;
+        }
+
+        if (subStr.length() == 0) {
+            return 0;
+        }
+
+        for (int i = 0; i <= primaryStr.length() - subStr.length(); i++) {
+            int j;
+
+            for (j = 0; j < subStr.length(); j++) {
+                if (subStr.charAt(j) != primaryStr.charAt(i + j)) {
+                    break;
+                }
+            }
+
+            if (j == subStr.length()) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }
--- src/test/java/string/StringxTest.java
+++ src/test/java/string/StringxTest.java
@@ -11,4 +11,12 @@ public class StringxTest {
         assertEquals("namtaB", Stringx.reverse("Batman"));
         assertEquals("namrepuS dna namtaB", Stringx.reverse("Batman and Superman"));
     }
+
+    @Test
+    public void testStrstrIterative() {
+        assertEquals(0, Stringx.strstrIterative("Batman", "Bat"));
+        assertEquals(1, Stringx.strstrIterative("Batman", "atm"));
+        assertEquals(-1, Stringx.strstrIterative("Batman and Superman", "Robin"));
+        assertEquals(11, Stringx.strstrIterative("Batman and Superman", "Super"));
+    }
 }

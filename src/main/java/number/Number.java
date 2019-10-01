20d409b02a6f9cfb245afdb762943cc0fe0a0346
Matt Rowles <matthewrowles@gmail.com>
2019-10-01 17:46:15 +1000

    Simplifies number problems

--- src/main/java/number/Number.java
+++ src/main/java/number/Number.java
@@ -1,5 +1,8 @@
 package number;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Number {
     public static int reverseInt(int n) {
         int reversed = 0;
@@ -11,5 +14,38 @@ public class Number {
         }
 
         return reversed;
+    }
+
+    public static List<Integer> findPrimeNumbers(int[] data) {
+        List<Integer> primeList = new ArrayList<>();
+
+        if (data.length == 0) {
+            throw new ArrayIndexOutOfBoundsException("Zero numbers passed");
+        }
+
+        for (int possiblePrimeNumber : data) {
+
+            if (possiblePrimeNumber > 1) {
+                boolean isPrime = true;
+                int count = 0;
+
+                for (int divisor = 1; divisor <= possiblePrimeNumber; divisor++) {
+                    if (possiblePrimeNumber % divisor == 0) {
+                        count++;
+                    }
+
+                    if (count > 2) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime) {
+                    primeList.add(possiblePrimeNumber);
+                }
+            }
+        }
+
+        return primeList;
     }
 }
--- src/test/java/number/NumberTest.java
+++ src/test/java/number/NumberTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import java.util.List;
+
 public class NumberTest {
 
     @Test
@@ -27,5 +29,18 @@ public class NumberTest {
         expected = 6666661;
         result = Number.reverseInt(1666666);
         assertEquals(expected, result);
+    }
+
+    @Test
+    public void findPrimeNumbers() {
+        int[] data = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+        List<Integer> results = Number.findPrimeNumbers(data);
+
+        assertEquals(2, results.get(0).intValue());
+        assertEquals(3, results.get(1).intValue());
+        assertEquals(5, results.get(2).intValue());
+        assertEquals(7, results.get(3).intValue());
+        assertEquals(11, results.get(4).intValue());
     }
 }

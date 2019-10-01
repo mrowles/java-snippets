703e8d255cd99f14319082ad98e57d5588f853f1
Matt Rowles <matthewrowles@gmail.com>
2019-10-02 07:22:49 +1000

    Adds 'abc' count algo

---
+++ src/main/java/abcCount/AbcCount.java
@@ -0,0 +1,42 @@
+package abcCount;
+
+public class AbcCount {
+    // length = n, aCount < n, bCount <= 1, cCount <=2
+    public static int findCombinations(int n) {
+        int aCount;
+        int bCount;
+        int cCount;
+        int result = 0;
+
+        for (int a = 0; a <= n; a++) {
+            aCount = a;
+
+            for (int b = 0; b <= n - aCount; b++) {
+                bCount = b;
+
+                if (bCount > 1) {
+                    continue;
+                }
+
+                cCount = n - (aCount + bCount);
+
+                if (cCount > 2) {
+                    continue;
+                }
+
+                System.out.println(aCount + " " + bCount + " " + cCount);
+                result = result + (factorial(n) / (factorial(aCount) * factorial(bCount) * factorial(cCount)));
+            }
+        }
+
+        return result;
+    }
+
+    private static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+}
---
+++ src/test/java/abcCount/AbcCountTest.java
@@ -0,0 +1,27 @@
+package abcCount;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AbcCountTest {
+
+    @Test
+    public void findCombinations() {
+        int expected = 3;
+        int results = AbcCount.findCombinations(1);
+        assertEquals(expected, results);
+
+        expected = 8;
+        results = AbcCount.findCombinations(2);
+        assertEquals(expected, results);
+
+        expected = 19;
+        results = AbcCount.findCombinations(3);
+        assertEquals(expected, results);
+
+        expected = 39;
+        results = AbcCount.findCombinations(4);
+        assertEquals(expected, results);
+    }
+}
